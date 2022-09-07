e8abfc23a3301b696adb4efbf0011b863e4a9faa
Niklas Eklund <niklas.eklund@posteo.net>
2022-09-07 13:52:54 +0200

    Configure detached-init to initialize sessions

--- detached.el
+++ detached.el
@@ -1120,13 +1120,11 @@ Optionally make the path LOCAL to host."
 
 (defun detached--db-insert-entry (session)
   "Insert SESSION into `detached--sessions' and update database."
-  (detached-initialize-sessions)
   (push `(,(detached--session-id session) . ,session) detached--sessions)
   (detached--db-update-sessions))
 
 (defun detached--db-remove-entry (session)
   "Remove SESSION from `detached--sessions', delete log and update database."
-  (detached-initialize-sessions)
   (let ((log (detached--session-file session 'log)))
     (when (file-exists-p log)
       (delete-file log)))
@@ -1136,24 +1134,20 @@ Optionally make the path LOCAL to host."
 
 (defun detached--db-update-entry (session &optional update)
   "Update SESSION in `detached--sessions' optionally UPDATE database."
-  (detached-initialize-sessions)
   (setf (alist-get (detached--session-id session) detached--sessions) session)
   (when update
     (detached--db-update-sessions)))
 
 (defun detached--db-get-session (id)
   "Return session with ID."
-  (detached-initialize-sessions)
   (alist-get id detached--sessions))
 
 (defun detached--db-get-sessions ()
   "Return all sessions stored in the database."
-  (detached-initialize-sessions)
   (seq-map #'cdr detached--sessions))
 
 (defun detached--db-update-sessions ()
   "Write `detached--sessions' to database."
-  (detached-initialize-sessions)
   (let ((db (expand-file-name "detached-sessions.db" detached-db-directory)))
     (with-temp-file db
       (insert (format ";; Detached Session Version: %s\n\n" detached-session-version))
