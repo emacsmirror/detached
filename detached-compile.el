00bf84e8f55ae7bcd015cf058ea1274f8e5dac17
Niklas Eklund <niklas.eklund@posteo.net>
2022-11-22 15:23:15 +0100

    Update start of detached compile
    Only register session if it hasn't already been started.

--- detached-compile.el
+++ detached-compile.el
@@ -134,7 +134,8 @@ Optionally EDIT-COMMAND."
                                              (detached-session-start-command
                                               detached-current-session
                                               :type 'string))
-          (detached-register-session detached-current-session)
+          (unless (detached-session-started-p detached-current-session)
+            (detached-register-session detached-current-session))
           (apply compilation-start `(,(if (detached-session-active-p detached-current-session)
                                           (detached-session-attach-command detached-current-session
                                                                            :type 'string)
